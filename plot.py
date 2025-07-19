a8793473f7eeb5a044056630600890353daf4f85
Sohambasu07 <soham.basu07@gmail.com>
2025-07-19 12:35:02 +0530

    feat: Add Pareto Front plot

--- plot.py
+++ plot.py
@@ -15,6 +15,54 @@ MARKERS = [
 ]
 
 
+def pareto_front(
+    costs: np.array,
+) -> np.array:
+    """Function to calculate the pareto front from a pandas Series
+    of Results, i.e., Mapping[str, float] objects.
+    """
+    is_pareto = np.ones(costs.shape[0], dtype=bool)
+    for i, c in enumerate(costs):
+        if is_pareto[i]:
+            is_pareto[is_pareto] = np.any(costs[is_pareto] < c, axis=1)
+            is_pareto[i] = True
+    return is_pareto
+
+
+def plot_pareto_front(
+    path: Union[str, Path],
+    objectives: Tuple[str, str] = ("objective_1", "objective_2")
+) -> None:
+    """Plot the pareto front from a given path."""
+    if isinstance(path, str):
+        path = Path(path)
+    assert path.is_dir(), f"Path {path} is not a directory"
+    assert (path / "summary" / "full.csv").exists(), \
+        f"Path {path} does not contain a summary/full.csv file. " \
+        f"Either incomplete run or parameter not set."
+    
+    # Load data
+    _df = pd.read_csv(path / "summary" / "full.csv")
+    costs = np.vstack(_df["objective_to_minimize"].values)
+    assert costs.shape[1] == 2, \
+        "Can only plot pareto front for 2 objectives"
+    is_pareto = pareto_front(costs)
+    pareto_costs = costs[is_pareto]
+    pareto_costs = pareto_costs[np.argsort(pareto_costs[:, 0])]
+
+    # Plot
+    plt.step(
+        pareto_costs[:, 0], 
+        pareto_costs[:, 1],
+        where="post",
+    )
+    plt.xlabel(objectives[0])
+    plt.ylabel(objectives[1])
+    plt.title("Pareto Front")
+    plt.grid()
+    plt.tight_layout()
+
+
 def get_incumbent_trace_over_time(
     path: Union[str | Path] = None,
     use_neps_cost: bool = False,
@@ -78,7 +126,6 @@ def get_args() -> argparse.Namespace:
         "--algos",
         nargs="+",
         type=str,
-        required=True,
         help="The list of algos to compare. Pass the output folder name that represents the algo.",
     )
     parser.add_argument(
@@ -106,6 +153,14 @@ def get_args() -> argparse.Namespace:
         default="incumbent_trace.png",
         help="Filename to save the plot."
     )
+    parser.add_argument(
+        "--do",
+        default="incumbent_trace",
+        choices=["incumbent_trace", "pareto_front"],
+        help="What to plot. "
+            "If 'incumbent_trace', plots the incumbent trace over time. "
+            "If 'pareto_front', plots the pareto front from the results."
+    )
     args = parser.parse_args()
     return args
 
@@ -116,38 +171,52 @@ if __name__ == "__main__":
     # Checks
     args.root_directory = Path(args.root_directory)
     assert args.root_directory.exists(), f"Path {args.root_directory} does not exist!"
-    for algo in args.algos:
-        assert (args.root_directory / algo).exists(), \
-            f"Path {args.root_directory / algo} does not exist!"
 
-    # Plot
+
     plt.figure(figsize=(8, 5))
-    for i, algo in enumerate(args.algos):
-        inc_performance, cost = get_incumbent_trace_over_time(
-            args.root_directory / algo,
-            use_neps_cost=args.use_neps_cost,
-            reduce_footprint=True
-        )
-        plt.step(
-            cost,
-            inc_performance,
-            label=algo,
-            marker=MARKERS[i],
-            markersize=6,
-            linewidth=2,
-            where="post"
-        )
-    if args.log_x:
-        plt.xscale("log")
-    if args.log_y:
-        plt.yscale("log")
-    plt.xlabel("Wallclock time (in s)", fontsize=15)
-    plt.ylabel("Loss", fontsize=15)
-    # Formatting y-axis
-    formatter = ticker.FuncFormatter(lambda y, _: '{:.2e}'.format(y))
-    plt.gca().yaxis.set_major_formatter(formatter)
-    plt.legend(prop={"size": 15})
-    plt.tight_layout()
+    match args.do:
+        case "pareto_front":
+            plot_pareto_front(
+                args.root_directory,
+            )
+        case "incumbent_trace":
+            assert args.algos is not None, "Please specify the algos to compare."
+            for algo in args.algos:
+                assert (args.root_directory / algo).exists(), \
+                    f"Path {args.root_directory / algo} does not exist!"
+
+            # Plot
+            for i, algo in enumerate(args.algos):
+                inc_performance, cost = get_incumbent_trace_over_time(
+                    args.root_directory / algo,
+                    use_neps_cost=args.use_neps_cost,
+                    reduce_footprint=True
+                )
+                plt.step(
+                    cost,
+                    inc_performance,
+                    label=algo,
+                    marker=MARKERS[i],
+                    markersize=6,
+                    linewidth=2,
+                    where="post"
+                )
+            if args.log_x:
+                plt.xscale("log")
+            if args.log_y:
+                plt.yscale("log")
+            plt.xlabel("Wallclock time (in s)", fontsize=15)
+            plt.ylabel("Loss", fontsize=15)
+            # Formatting y-axis
+            formatter = ticker.FuncFormatter(lambda y, _: '{:.2e}'.format(y))
+            plt.gca().yaxis.set_major_formatter(formatter)
+            plt.legend(prop={"size": 15})
+            plt.tight_layout()
+        case _:
+            raise ValueError(
+                f"Unknown value for --do: {args.do}. "
+                f"Expected 'incumbent_trace' or 'pareto_front'."
+            )
 
     # Saving plot
     save_directory = (
